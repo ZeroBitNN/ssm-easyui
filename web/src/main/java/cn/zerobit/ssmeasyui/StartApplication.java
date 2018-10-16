9ae5b4aeaff2bc712effe69d9e51358d28d25c7b
zerobit <leonngx@hotmail.com>
2018-10-16 16:54:41 +0800

    Initial commit

---
+++ web/src/main/java/cn/zerobit/ssmeasyui/StartApplication.java
@@ -0,0 +1,22 @@
+package cn.zerobit.ssmeasyui;/**
+ * @author ZeroBit
+ * @create 2018-10-16 16:26
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName StartApplication
+ * @Description TODO
+ * @Author ZeroBit
+ * @Date 2018/10/16
+ **/
+
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
